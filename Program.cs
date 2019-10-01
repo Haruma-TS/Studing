7eda2daefb57d2da96b8c4fdecd9cbab7b36a029
Александр Соловьев <mrnakezz@gmail.com>
2019-10-01 20:01:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new List<int>();
+            for(var i = 0;i<10;i++)
+            {
+                collection.Add(i);
+            }
+
+        }
+    }
+}
